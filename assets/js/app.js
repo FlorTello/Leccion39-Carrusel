f7e96e8a8bf80f345bc28692a5ac68bfdbed899d
Flor <flor.26071994@gmail.com>
2017-06-05 01:42:06 -0500

    Agregando funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,43 @@
-// $(document).ready(function(){
-//   alert('Cargo JQuery');
-// });
-//
-// $( function(){
-//   alert('Cago JQuery');
-// });
-//
-// $( =>{
-//   alert('Cago JQuery');
-// });
+$(() =>  {
 
-$( () =>{
-  $("a:first-child").click((e)=>{
-    e.preventDefault();
-    $(".brand-popup").show();
-    // $(".brand-popup").removeClass('hide');
-    // $(".brand-popup").addClass('show');
-  });
-  $("a:last-child").click((e)=>{
-    e.preventDefault();
-    $(".brand-popup").toggle();
-    // $(".brand-popup").removeClass('hide');
-    // $(".brand-popup").addClass('show');
-  });
-  $(".popup-link").on('click',(e) => {
-  e.preventDefault();
-  $('.brand-popup').show();
-  //$('.brand-popup').removeClass('hide');
-  //$('.brand-popup').addClass('show');
+  // Obteniendo los el ancho y el alto de todas las li que contienen la imagen
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+  // Funcion de auto play
+  setInterval(() => {moveRight(); }, 5000);
 
-  });
+	$('#slider').css({ width: slideWidth, height: slideHeight });
 
-  $(".brand-popup .popup-close").on('click',(e) => {
-    e.preventDefault();
-    $('.brand-popup').hide();
-    //$('.brand-popup').removeClass('show');
-    //$('.brand-popup').addClass('hide');
-  });
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+  $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('div.btn-prev').click(function () {
+        moveLeft();
+    });
+
+    $('div.btn-next').click(function () {
+        moveRight();
+    });
 
 });
